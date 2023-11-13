1fa856c06688703d713c8e2491b377a8b458be16
Steph <stazatorres@hotmail.com>
2023-11-13 18:25:14 +0000

    Revert "playing"
    This reverts commit eaf2a95a99bddc90c595b3cae7bf2ae7c68431d0.

--- react-to-do-list/src/FilterItem.jsx
+++ react-to-do-list/src/FilterItem.jsx
@@ -5,30 +5,28 @@ import { mdiDeleteOffOutline } from "@mdi/js";
 function FilteredItem({ item, handleDelete }) {
   return (
     <div>
-      {item.length !== 0
-        ? item.map((item) => {
-            return (
-              <div className="filtered_item" key={item.id}>
-                <h3 style={{ textDecoration: "underline" }}>{item.title}</h3>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                  }}
-                >
-                  <p>Is complete: {`${item.isComplete}`}</p>
-                  <Icon
-                    path={mdiDeleteOffOutline}
-                    size={1}
-                    className="icon"
-                    onClick={() => handleDelete(item.id)}
-                  ></Icon>
-                </div>
-              </div>
-            );
-          })
-        : null}
+      {item.map((item) => {
+        return (
+          <div className="filtered_item" key={item.id}>
+            <h3 style={{ textDecoration: "underline" }}>{item.title}</h3>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <p>Is complete: {`${item.isComplete}`}</p>
+              <Icon
+                path={mdiDeleteOffOutline}
+                size={1}
+                className="icon"
+                onClick={() => handleDelete(item.id)}
+              ></Icon>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- react-to-do-list/src/FilteredItemDisplay.jsx
+++ react-to-do-list/src/FilteredItemDisplay.jsx
@@ -2,18 +2,9 @@
 import FilteredItem from "./FilterItem";
 
 function FilteredItemDisplay({ arr, handleDelete }) {
-  function removeExtra(array) {
-    for (let i = 0; i < array.length; i++) {
-      if (array[i].length === 0) {
-        array.splice(array.indexOf(array[i]), 1);
-      }
-    }
-  }
-
-  const filter = removeExtra(arr);
   return (
     <div className="card-display">
-      {filter.map((item) => {
+      {arr.map((item) => {
         return (
           <FilteredItem key={item.id} item={item} handleDelete={handleDelete} />
         );
