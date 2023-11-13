fb43e4194da58ac81d2d1cabcbb44c4762a15ad9
Steph <stazatorres@hotmail.com>
2023-11-13 17:53:39 +0000

    bug fix: filtered items would not always appear at the beginning of the page depending if a previous toDoCard had tasks within it or not. for loop ln184 of App sorts this out by removing any empty arrays before it is returned

--- react-to-do-list/src/App.jsx
+++ react-to-do-list/src/App.jsx
@@ -179,7 +179,13 @@ function App() {
         filteredTasks.push(additions);
       }
     });
-
+    console.log(filteredTasks);
+    /* this makes sure no empty arrays are passed down/forward, so filtered obj don't appear in weird positions */
+    for (let i = 0; i < filteredTasks.length; i++) {
+      if (filteredTasks[i].length === 0) {
+        filteredTasks.splice(filteredTasks.indexOf(filteredTasks[i]), 1);
+      }
+    }
     return filteredTasks;
   }
 
