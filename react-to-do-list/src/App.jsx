eaf2a95a99bddc90c595b3cae7bf2ae7c68431d0
Steph <stazatorres@hotmail.com>
2023-11-13 18:22:11 +0000

    playing

--- react-to-do-list/src/App.jsx
+++ react-to-do-list/src/App.jsx
@@ -179,13 +179,16 @@ function App() {
         filteredTasks.push(additions);
       }
     });
-    console.log(filteredTasks);
+    
+    
     /* this makes sure no empty arrays are passed down/forward, so filtered obj don't appear in weird positions */
+    
     for (let i = 0; i < filteredTasks.length; i++) {
       if (filteredTasks[i].length === 0) {
         filteredTasks.splice(filteredTasks.indexOf(filteredTasks[i]), 1);
       }
     }
+    console.log(filteredTasks);
     return filteredTasks;
   }
 
--- react-to-do-list/src/FilteredItemDisplay.jsx
+++ react-to-do-list/src/FilteredItemDisplay.jsx
@@ -2,9 +2,18 @@
 import FilteredItem from "./FilterItem";
 
 function FilteredItemDisplay({ arr, handleDelete }) {
+  function removeExtra(array) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].length === 0) {
+        array.splice(array.indexOf(array[i]), 1);
+      }
+    }
+  }
+
+  const filter = removeExtra(arr);
   return (
     <div className="card-display">
-      {arr.map((item) => {
+      {filter.map((item) => {
         return (
           <FilteredItem key={item.id} item={item} handleDelete={handleDelete} />
         );
