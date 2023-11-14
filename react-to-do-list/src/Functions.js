d95df8a1172e2562e06f2b71cfa8b2fe60b6b0bb
Sebastien Torres <storres@numarkdigital.co.uk>
2023-11-14 08:38:27 +0000

    Use greater than 0 as the comparison.
    This is a question of readability, context and numerical comparison...
    An empty list has 0 elements, thus a non-empty list has non-0 elements; a non-0 positive value.
    
    Logically for the presence of something isn't that there are no items; length != 0; but rather
    that there are items and as such, length of the array is greater than 0;
    The context of comparison is the count of items has to be a positive value.
    
    For readability, where possible, making a positive statement is easier to read and write than making a negative
    statement to determine a truth.

--- react-to-do-list/src/Functions.js
+++ react-to-do-list/src/Functions.js
@@ -1,9 +1,9 @@
 export function searchTaskByTitle(title, toDoList) {
   const filteredTasks = [];
   toDoList.forEach((toDo) => {
-    if (toDo.list.length !== 0) {
+    if (toDo.list.length > 0) {
       let additions = toDo.list.filter((item) => item.title.includes(title));
-      if (additions.length !== 0) {
+      if (additions.length > 0) {
         filteredTasks.push(additions);
       }
     }
